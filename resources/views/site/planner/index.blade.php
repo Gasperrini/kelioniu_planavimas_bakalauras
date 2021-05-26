72d2752c5062204b9035392c54260caaa58f235b
Ignas <ign.gasparavicius@gmail.com>
2021-05-26 10:27:48 +0300

    heroku

--- resources/views/site/planner/index.blade.php
+++ resources/views/site/planner/index.blade.php
@@ -38,7 +38,7 @@
                   foreach($segments as $segment){
                     if($segment->route_id == $code){
                       echo '["'.$segment->name.'", '.$segment->lat.', '.$segment->lng.'],';
-                      dd("gerai");
+                      dd($segment->lat);
                     }
                     else dd("negerai");
                   }
